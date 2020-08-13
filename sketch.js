14ee49f64524e9595c84b150806dd777b7abedc0
tetunori <tetunori@gmail.com>
2020-08-13 22:46:10 +0900

    Enable display test picture

--- sketch.js
+++ sketch.js
@@ -1,47 +1,82 @@
 
 let gPict;
-let c;
+
+const CELL_SIZE = 10;
 
 const ROWS = 56;
 const COLUMNS = 72;
-const CELL_SIZE = 10;
-const LINE_LEVELS = 5;
+
+
 
 function preload() {
   gPict = loadImage('images/testPict01.png');
-}
+}  
+
 
 function setup() {
-  createCanvas(COLUMNS * CELL_SIZE, ROWS * CELL_SIZE);
-  image(gPict, 0, 0);
+  createCanvas(gPict.width, gPict.height);
+
+  stroke( "#BAAFA7" );
+  for(let j = 0; j < height; j += CELL_SIZE){
+    for(let i = 0; i < width; i += CELL_SIZE){
+      rect(i, j, CELL_SIZE, CELL_SIZE);
+    }
+  }
 
 
-  
+
+  // image(gPict, 0, 0);
   gPict.filter(GRAY);
 
   noStroke();
+  noFill();
   for(let j = 0; j < height; j += CELL_SIZE){
     for(let i = 0; i < width; i += CELL_SIZE){
-      c = gPict.get(i, j);
+      const color = gPict.get(i, j);
+      const grayScaleColor = color[0];
       //  console.log(c);
       
-      fill(50 * getLineLevel(c[0]));
+      // fill(51 * getLineLevel(c[0]));
       // fill(c);
-      rect(i, j, CELL_SIZE, CELL_SIZE);
+      stroke('#202020');
+      if( getLineLevel(grayScaleColor) === 1 ){
+        line(i, j,  i+CELL_SIZE, j);
+        line(i, j+CELL_SIZE,  i+CELL_SIZE, j+CELL_SIZE);
+      }else if( getLineLevel(grayScaleColor) === 2 ){
+        rect(i, j, CELL_SIZE, CELL_SIZE);   
+      }else if( getLineLevel(grayScaleColor) === 3 ){
+        rect(i, j, CELL_SIZE, CELL_SIZE);
+        line(i, j,  i+CELL_SIZE, j+CELL_SIZE);
+      }else if( getLineLevel(grayScaleColor) === 4 ){
+        rect(i, j, CELL_SIZE, CELL_SIZE);
+        line(i, j,  i+CELL_SIZE, j+CELL_SIZE);
+        line(i+CELL_SIZE, j,  i, j+CELL_SIZE);
+      }
     }
  }
-  //image(gPict, width/2, 0);
+
+
+  stroke( 'black' );
+  strokeWeight(2.2);
+  line(0, height/4, width, height/4);
+  line(0, height/2, width, height/2);
+  line(0, 3 * height/4, width, 3 * height/4);
+
+  line(width/4, 0, width/4, height);
+  line(width/2, 0, width/2, height);
+  line(3 * width/4, 0, 3 * width/4, height);
+
 }
 
 const getLineLevel = ( grayScaleValue ) => {
 
-  const lineLevel = Math.floor( (grayScaleValue / 255) * LINE_LEVELS );
+  const LINE_LEVELS = 5;
+
+  // line level ranges from 0 to 4.
+  const lineLevel = 4 - Math.floor( (grayScaleValue / 256) * LINE_LEVELS );
 
   // console.log(grayScaleValue, lineLevel);
   return lineLevel;
 
 }
 
-// function draw() {
-//   background(220);
-// }
