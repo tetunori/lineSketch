49fcf24e947f76c08206783a66ad131d84531659
tetunori <tetunori@gmail.com>
2020-08-14 11:17:54 +0900

    Support controllers for smart devices

--- sketch.js
+++ sketch.js
@@ -10,7 +10,13 @@ const CELL_SIZE = 10;
 // Line level ranges from 0 to 4.
 const LINE_LEVEL_MAX = 4;
 
+// For controller
+let gFileSelector;
+let gBtToggleGrid = undefined;
+let gBtToggleQuarterLine = undefined;
+
 function setup() {
+
 
   // Prepare canvas
   const canvas = createCanvas( 800, 800 )
@@ -30,7 +36,18 @@ function setup() {
 
   // Prepare drag&drop.
   canvas.drop( onImageDropped );
+  
+  // Prepare controllers
+  gFileSelector = createFileInput( onImageDropped );
 
+  gBtToggleGrid = createButton('Toggle Grid');
+  gBtToggleGrid.mousePressed( toggleGrid );
+
+  gBtToggleQuarterLine = createButton('Toggle Quarter lines');
+  gBtToggleQuarterLine.mousePressed( toggleQuarterLine );
+
+  // Redraw controller
+  redrawControllers();
 }
 
 // Callback for image dropped.
@@ -56,9 +73,11 @@ const drawDropCanvas = () => {
 
   background( 200 );
   textAlign( CENTER );
-  textSize( 26 );
+  textSize( 22 );
   text( 'Drop image HERE! \n\n The larger the pixel size of an image, \n the longer the analysis time', width / 2, height / 2 );
 
+  redrawControllers();
+  
 }
 
 // Draw whole line sketch. Main procedure.
@@ -82,6 +101,9 @@ const drawLineSketch = () => {
 
   // Draw Quater lines finally.
   drawQuarterLine();
+
+  // Redraw controller
+  redrawControllers();
 
 }
 
@@ -189,6 +211,7 @@ const getLineLevel = ( grayScaleValue ) => {
 
 }
 
+// Support Key press commands
 function keyPressed() {
 	
   if( key === 's' || key === 'S' ) {
@@ -196,15 +219,41 @@ function keyPressed() {
     save();
   
   }else if( key === 'z' || key === 'Z' ) {
-    
+    toggleGrid();
+  }else if( key === 'x' || key === 'X' ) {
+    toggleQuarterLine();
+  }
+
+}
+
+// Toggle Grid
+const toggleGrid = () => {
+
     gToggleGrid = !gToggleGrid;
     drawLineSketch();
   
-  }else if( key === 'x' || key === 'X' ) {
-  
+}
+
+// Toggle quarter lines
+const toggleQuarterLine = () => {
+
     gToggleQuarterLine = !gToggleQuarterLine;
     drawLineSketch();
   
+}
+
+const redrawControllers = () => {
+
+  if( gBtToggleGrid ){
+    gBtToggleGrid.position( 10, height + 10 );
+  }
+
+  if( gBtToggleQuarterLine ){
+    gBtToggleQuarterLine.position( 110, height + 10 );
+  }
+
+  if( gFileSelector ){
+    gFileSelector.position( 270, height + 10 );
   }
 
 }
